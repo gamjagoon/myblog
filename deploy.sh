9513723764e34fbf2eddb62c72db1e71b7f09739
minjae <rlaalswo201@gmail.com>
2022-06-06 15:08:08 +0900

    rebuilding site 2022년 6월  6일 월요일 15시 08분 08초 KST

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+hugo --minify
+cd public
+git pull origin main
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
+
+cd ..
+git pull origin main 
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
